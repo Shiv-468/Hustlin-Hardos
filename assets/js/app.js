df3fdbc72358f741a4b6c99dce4d830b22b786c9
Shiv-468 <shivmuwal.147@gmail.com>
2024-03-15 13:47:57 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+$('.slider_1').slick({
+    dots: false,
+    infinite: true,
+    speed: 2000,
+    arrows: false,
+    autoplay:true,
+    slidesToShow: 7,
+    autoplaySpeed:0,
+    slidesToScroll: 1,
+    varialbleWidth:true,
+    cssEase:'linear',
+    pauseOnHover: true
+});

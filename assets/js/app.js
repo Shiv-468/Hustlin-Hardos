9ba141538b8bd7605945794fbc77f9cd733ecca1
Shiv-468 <shivmuwal.147@gmail.com>
2024-03-19 10:52:57 +0530

    add meta tags

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,46 +3,51 @@ $('.slider_1').slick({
     infinite: true,
     speed: 2000,
     arrows: false,
-    autoplay:true,
+    autoplay: true,
     slidesToShow: 7,
-    autoplaySpeed:0,
+    autoplaySpeed: 0,
     slidesToScroll: 1,
-    varialbleWidth:true,
-    cssEase:'linear',
+    varialbleWidth: true,
+    cssEase: 'linear',
     pauseOnHover: true,
     responsive: [
         {
             breakpoint: 1062,
             settings: {
-                slidesToShow: 6,
+                slidesToShow: 5,
                 slidesToScroll: 1,
             }
         },
         {
             breakpoint: 992,
             settings: {
-                slidesToShow: 5,
+                slidesToShow: 4,
                 slidesToScroll: 2
             }
         },
         {
             breakpoint: 768,
             settings: {
-                slidesToShow: 4,
+                slidesToShow: 3,
                 slidesToScroll: 1
             }
         },
         {
             breakpoint: 576,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                slidesToScroll: 1,
+                varialbleWidth: true,
+                cssEase: 'linear',
+                pauseOnHover: true,
             }
+
         },
         {
             breakpoint: 328,
             settings: {
-                slidesToShow: 2,
+                slidesToShow: 1.2,
                 slidesToScroll: 1
             }
         }
@@ -53,9 +58,9 @@ $('.slider_2').slick({
     infinite: true,
     speed: 300,
     arrows: true,
-    prevArrow:".left_arrow",
-    nextArrow:".right_arrow",
-    autoplay:false,
+    prevArrow: ".left_arrow",
+    nextArrow: ".right_arrow",
+    autoplay: false,
     slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
@@ -67,17 +72,18 @@ $('.slider_2').slick({
             }
         },
         {
-            breakpoint:964,
-            settings:{
-                slidesToShow:2,
-                slidesToScroll:1,
+            breakpoint: 964,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
             }
         },
         {
-            breakpoint:564,
-            settings:{
-                slidesToShow:1,
-                slidesToScroll:1,
+            breakpoint: 564,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                speed: 300,
             }
         }
     ]
@@ -87,9 +93,9 @@ $('.slider_3').slick({
     infinite: true,
     speed: 300,
     arrows: true,
-    prevArrow:".left_arrow2",
-    nextArrow:".right_arrow2",
-    autoplay:false,
+    prevArrow: ".left_arrow2",
+    nextArrow: ".right_arrow2",
+    autoplay: false,
     slidesToShow: 2,
     slidesToScroll: 1,
     responsive: [
@@ -128,4 +134,35 @@ accordion.forEach((e) => {
 
     })
 
-})
+});
+
+var countDownDate = new Date("Apr 27, 2024 18:37:25").getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("demo").innerHTML = days + ":" + hours + ":"
+        + minutes + ":" + seconds;
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+const menuicon = document.querySelector(".menuicon");
+const menulist = document.querySelector(".menulist");
+menuicon.addEventListener("click", mobileMenu);
+const Navlink = document.querySelectorAll(".nav-link");
+menulist.forEach(e => e.addEventListener("click", () => {
+    menuicon.classList.toggle("active");
+    document.body.classList.remove("overflow-hidden");
+    menulist.classList.toggle("active");
+}))
+function mobileMenu() {
+    menuicon.classList.toggle("active");
+    document.body.classList.toggle("overflow-hidden");
+    menulist.classList.toggle("active");
+}
